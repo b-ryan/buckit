1158a12635e2501209e68315d4c35633477332e6
Buck Ryan <buck.ryan@gmail.com>
2013-08-30 00:26:23 -0400

    seed data

--- src/budget.py
+++ src/budget.py
@@ -6,11 +6,15 @@ import config
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--migrate', action='store_true')
+parser.add_argument('--seed', action='store_true')
 args = parser.parse_args()
 
 if args.migrate:
     import model
     from model.base import Base
     Base.metadata.create_all(config.engine)
+elif args.seed:
+    import seed
+    seed.seed()
 else:
     bottle.run(port=config.port, reloader=config.use_reloader)
---
+++ src/seed.py
@@ -0,0 +1,22 @@
+import model
+import config
+
+def seed():
+    session = config.Session()
+
+    account = model.Account(name='Primary Checking')
+
+    splits = [
+        model.Split(amount=23),
+        model.Split(amount=0.5),
+    ]
+    trans1 = model.Transaction(
+        account=account,
+        date='2013-08-30',
+        status='not_reconciled',
+        splits=splits,
+    )
+
+    session.add(account)
+    session.add(trans1)
+    session.commit()
