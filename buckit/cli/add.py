1c10229f08745b3edf889bf1cd9cc10b9c5267b2
Buck Ryan <buck.ryan@gmail.com>
2013-09-27 20:49:10 -0400

    adding transactions works!

--- buckit/cli/add.py
+++ buckit/cli/add.py
@@ -2,7 +2,7 @@ import exception
 from buckit.config import date_format
 from buckit.utils import with_session
 import buckit.model as m
-import datetime
+from datetime import date, datetime
 
 def setup_parser(parent_parser):
     parser = parent_parser.add_parser('add')
@@ -10,27 +10,42 @@ def setup_parser(parent_parser):
 
     p = subs.add_parser('transaction')
     p.add_argument('--date', '-d',
-        default=datetime.date.today().strftime(date_format),
+        default=date.today().strftime(date_format),
     )
     p.add_argument('--payee', '-p')
-    p.add_argument('--account', '-a', required=True)
-    p.add_argument('--recipient', '-r', required=True)
-    p.add_argument('--amount', '-x', required=True)
+    p.add_argument('--from-account', '-f', required=True)
+    p.add_argument('--to-account', '-t', required=True)
+    p.add_argument('--amount', '-a', required=True)
     p.set_defaults(func=add_transaction)
+
+def search_by_name(session, model, name):
+    result = session.query(model).filter_by(name=name).first()
+    if result is not None:
+        return result
+    raise exception.CliException('Unknown: ' + name)
 
 @with_session
 def add_transaction(session, args):
+    transaction = m.Transaction()
+
     try:
-        date = datetime.datetime.strptime(args.date, date_format).date()
+        transaction.date = datetime.strptime(args.date, date_format).date()
     except ValueError:
         msg = 'Invalid date format'
         raise exception.CliException(msg)
 
-    payee = None
-    if args.payee is not None:
-        payee = session.query(m.Payee).filter_by(name=args.payee).first()
-        if payee is None:
-            raise exception.CliException('Unknown payee ' + args.payee)
+    if args.payee:
+        transaction.payee = search_by_name(session, m.Payee, args.payee)
 
-    print date
-    print payee
+    from_account = search_by_name(session, m.Account, args.from_account)
+    to_account = search_by_name(session, m.Account, args.to_account)
+
+    transaction.splits = [
+        m.Split(account=from_account, amount=float(args.amount) * -1),
+        m.Split(account=to_account,   amount=float(args.amount)),
+    ]
+
+    session.add(transaction)
+    session.commit()
+
+    print transaction
