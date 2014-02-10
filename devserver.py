8f22b5e13f95687aebb38556d2202098816eba47
Buck Ryan <buck.ryan@gmail.com>
2014-02-09 23:05:25 -0500

    have devserver.py compile coffee for you

--- devserver.py
+++ devserver.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 from buckit.app import app
+import subprocess
+import sys
 
 if __name__ == '__main__':
+    subprocess.Popen(['coffee', '-cwo', 'public/.compiled/', 'public/coffee'],
+                     stdout=sys.stdout, stderr=sys.stderr)
+
     app.static_folder = '../public'
 
     @app.route('/', defaults={'filename': 'index.html'})
