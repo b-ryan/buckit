14defcb6abc09a2c8acdf52ae7dac8da040b6d94
Buck Ryan <buck.ryan@gmail.com>
2014-07-06 15:09:33 -0400

    pep8 conformance

--- buckit/models.py
+++ buckit/models.py
@@ -6,31 +6,31 @@ from sqlalchemy.schema import UniqueConstraint
 Base = declarative_base()
 
 AccountType = Enum(
-    'liability', # like a credit card
-    'asset', # like a bank account
-    'income', # 'account' where money is entering your possession
-    'expense', # 'account' where money leaves your possession
-    'equity', # like stocks or options
+    'liability',  # like a credit card
+    'asset',  # like a bank account
+    'income',  # 'account' where money is entering your possession
+    'expense',  # 'account' where money leaves your possession
+    'equity',  # like stocks or options
     name='account_type',
 )
 
 
-"""
-Accounts encompass the concepts of both actual accounts, such as a
-checking or savings account, and categories, which can be either
-expense or income.
-"""
 class Account(Base):
+    """
+    Accounts encompass the concepts of both actual accounts, such as a
+    checking or savings account, and categories, which can be either
+    expense or income.
+    """
 
     __tablename__ = 'accounts'
 
-    id        = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     parent_id = Column(Integer, ForeignKey('accounts.id'))
-    name      = Column(String(255), nullable=False, unique=True)
-    type      = Column(AccountType, nullable=False)
+    name = Column(String(255), nullable=False, unique=True)
+    type = Column(AccountType, nullable=False)
 
     parent_account = relationship('Account')
-    splits         = relationship('Split')
+    splits = relationship('Split')
 
 ReconciledStatus = Enum(
     'not_reconciled',
@@ -44,19 +44,19 @@ class Payee(Base):
 
     __tablename__ = 'payees'
 
-    id        = Column(Integer, primary_key=True)
-    name      = Column(String(255), nullable=False, unique=True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
 
 
 class Transaction(Base):
 
     __tablename__ = 'transactions'
 
-    id         = Column(Integer, primary_key=True)
-    date       = Column(Date, nullable=False)
-    payee_id   = Column(Integer, ForeignKey('payees.id'))
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, nullable=False)
+    payee_id = Column(Integer, ForeignKey('payees.id'))
 
-    payee  = relationship('Payee')
+    payee = relationship('Payee')
     splits = relationship('Split')
 
 
@@ -64,15 +64,15 @@ class Split(Base):
 
     __tablename__ = 'transaction_splits'
 
-    id                 = Column(Integer, primary_key=True)
-    transaction_id     = Column(Integer, ForeignKey('transactions.id'),
-            nullable=False)
-    account_id         = Column(Integer, ForeignKey('accounts.id'),
-            nullable=False)
-    amount             = Column(Float, default=0.)
-    reconciled_status  = Column(ReconciledStatus, default='not_reconciled')
+    id = Column(Integer, primary_key=True)
+    transaction_id = Column(Integer, ForeignKey('transactions.id'),
+                            nullable=False)
+    account_id = Column(Integer, ForeignKey('accounts.id'),
+                        nullable=False)
+    amount = Column(Float, default=0.)
+    reconciled_status = Column(ReconciledStatus, default='not_reconciled')
 
-    account     = relationship('Account')
+    account = relationship('Account')
     transaction = relationship('Transaction', lazy=False)
 
     __table_args__ = (
--- buckit/routes.py
+++ buckit/routes.py
@@ -1,10 +1,11 @@
 from flask.ext.restless import APIManager
 import buckit.models as m
 
+
 def setup_routes(app, db):
     api = APIManager(app, flask_sqlalchemy_db=db)
     api.create_api(m.Account, methods=['GET'], results_per_page=None)
     api.create_api(m.Payee, methods=['GET'], results_per_page=None)
     api.create_api(m.Transaction, methods=['GET', 'POST'],
-            results_per_page=None)
+                   results_per_page=None)
     api.create_api(m.Split, methods=['POST'], results_per_page=None)
