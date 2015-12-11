7434463d30c47437283e22ea7a797b8783ff47f1
Buck Ryan <buck.ryan@gmail.com>
2015-12-10 21:59:25 -0500

    use blueprints

--- buckit/routes.py
+++ buckit/routes.py
@@ -1,12 +1,31 @@
-from flask.ext.restless import APIManager
 import buckit.models as m
+from flask.ext.restless import APIManager
 
 
 def initialize(app):
     api = APIManager(app, flask_sqlalchemy_db=app.db)
-    api.create_api(m.Account, methods=['GET', 'POST', 'PUT'],
-                   results_per_page=None)
-    api.create_api(m.Payee, methods=['GET', 'POST'], results_per_page=None)
-    api.create_api(m.Transaction, methods=['GET', 'POST', 'PUT'],
-                   results_per_page=None)
-    api.create_api(m.Split, methods=['POST'], results_per_page=None)
+
+    blueprints = [
+        api.create_api_blueprint(
+            m.Account,
+            methods=['GET', 'POST', 'PUT'],
+            results_per_page=None,
+        ),
+        api.create_api_blueprint(
+            m.Payee,
+            methods=['GET', 'POST'],
+            results_per_page=None,
+        ),
+        api.create_api_blueprint(
+            m.Transaction,
+            methods=['GET', 'POST', 'PUT'],
+            results_per_page=None,
+        ),
+        api.create_api_blueprint(
+            m.Split,
+            methods=['POST'],
+            results_per_page=None,
+        ),
+    ]
+
+    map(app.register_blueprint, blueprints)
