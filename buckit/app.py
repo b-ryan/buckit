3fed8103b666ccca2b6c7ed3d89dcc49cfe90e5f
Buck Ryan <buck.ryan@gmail.com>
2015-12-24 14:58:12 -0500

    some debugging helper functions

--- buckit/app.py
+++ buckit/app.py
@@ -1,6 +1,6 @@
 import buckit.routes
 import flask
-from flask.ext.cors import CORS
+import flask.ext.cors as cors
 from flask.ext.sqlalchemy import SQLAlchemy
 import logging
 import os
@@ -9,7 +9,7 @@ def create():
     app = flask.Flask(__name__, static_folder=None)
     app.logger.setLevel(logging.DEBUG)
 
-    CORS(app)
+    cors.CORS(app)
 
     db_url = 'sqlite:///{}/buckit.sqlite'.format(os.getcwd())
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url
--- buckit/routes.py
+++ buckit/routes.py
@@ -1,5 +1,18 @@
 import buckit.models as m
 from flask.ext.restless import APIManager
+import time
+
+
+def _delay(blueprint, seconds=3):
+    """Causes the request to be slow. Should only be used while developing."""
+    blueprint.before_request(lambda: time.sleep(seconds))
+
+
+def _fail(blueprint):
+    """Causes the request to fail. Should only be used while developing."""
+    def raise_it():
+        raise Exception()
+    blueprint.before_request(raise_it)
 
 
 def initialize(app):
@@ -19,13 +32,17 @@ def initialize(app):
         api.create_api_blueprint(
             m.Transaction,
             methods=['GET', 'POST', 'PUT'],
-            results_per_page=None,
+            results_per_page=25,
         ),
         api.create_api_blueprint(
             m.Split,
             methods=['POST'],
-            results_per_page=None,
+            results_per_page=25,
         ),
     ]
+
+    # _delay(blueprints[2])
+    # _delay(blueprints[0])
+    # _fail(blueprints[0])
 
     map(app.register_blueprint, blueprints)
